350e60c4bd0668e06f60ae283df4ba6647e9ca0a
John Preston <wcerfgba@riseup.net>
2021-02-06 22:54:23 +0000

    More experimentation with prime limits

--- src/new_harmony/core.clj
+++ src/new_harmony/core.clj
@@ -3,12 +3,12 @@
 
 (def primes [2 3 5])
 
-(def power-range 3)
+(def power-range 7)
 
-(def five-limit-maps
+(def prime-limit-maps
   (map (partial zipmap primes) (apply combi/cartesian-product (repeat (count primes) (range (- power-range) (inc power-range))))))
 
-(def five-limit-ratios
+(def prime-limit-ratios
   (map (partial reduce
                 (fn [acc [b x]]
                   (* acc (if (neg? x)
@@ -16,10 +16,21 @@
                            (/ (int (Math/pow b x))
                               1))))
                 1)
-       five-limit-maps))
+       prime-limit-maps))
 
-(def five-limits
-  (zipmap five-limit-maps five-limit-ratios))
+(def prime-limits
+  (zipmap prime-limit-maps prime-limit-ratios))
+
+(def prime-limit-complexities
+  (zipmap prime-limit-maps
+          (map (fn [m]
+                 (* (Math/pow (count (remove zero? (vals m))) 2)
+                    (reduce
+                     (fn [acc [b x]]
+                       (+ acc (int (Math/pow b (Math/abs x)))))
+                     1
+                     m)))
+               prime-limit-maps)))
 
 (def harmonic-series
   (concat [1] (take 32 (map / (drop 2 (range)) (drop 1 (range))))))
@@ -34,25 +45,26 @@
 (def twelve-tets
   (into {} (map #(vector % (Math/pow 2 (/ % 12))) (range 1 13))))
 
-(def twelve-tet->nearest-five-limit
-  (into {} (map (fn [[twelve-tet-idx twelve-tet-ratio]]
-                  [twelve-tet-idx (reduce (fn [{:keys [difference] :as nearest}
-                                               [five-limit-map five-limit-ratio]]
-                                            (let [new-difference (- twelve-tet-ratio
-                                                                    five-limit-ratio)]
-                                              (if (< (Math/abs new-difference) 
-                                                     (Math/abs difference))
-                                                {:map five-limit-map
-                                                 :ratio five-limit-ratio
-                                                 :difference new-difference}
-                                                nearest)))
-                                          {:difference Float/MAX_VALUE}
-                                          five-limits)])
-                twelve-tets)))
-
 (defn ratio->cents
   [r]
   (* 1200 (/ (Math/log r) (Math/log 2))))
+
+(def twelve-tet->nearest-prime-limit
+  (into {} (map (fn [[twelve-tet-idx twelve-tet-ratio]]
+                  [twelve-tet-idx (reduce (fn [{:keys [difference] :as nearest}
+                                               [prime-limit-map prime-limit-ratio]]
+                                            (let [new-difference (- (ratio->cents twelve-tet-ratio)
+                                                                    (ratio->cents prime-limit-ratio))]
+                                              (if (and (<= 6 (Math/abs difference))
+                                                       (< (Math/abs new-difference)
+                                                          (Math/abs difference)))
+                                                {:map prime-limit-map
+                                                 :ratio prime-limit-ratio
+                                                 :difference new-difference}
+                                                nearest)))
+                                          {:difference Float/MAX_VALUE}
+                                          (sort-by (comp prime-limit-complexities first) prime-limits))])
+                twelve-tets)))
 
 (def twelve-tet->nearest-harmonic-limit
   (into {} (map (fn [[twelve-tet-idx twelve-tet-ratio]]
@@ -60,28 +72,35 @@
                                                [i harmonic-limit]]
                                             (let [limit-differences
                                                   (zipmap harmonic-limit
-                                                          (map #(- (ratio->cents twelve-tet-ratio)
-                                                                   (ratio->cents %))
+                                                          (map #(/ twelve-tet-ratio
+                                                                   %)
                                                                harmonic-limit))
                                                   new-nearest
                                                   (reduce (fn [{:keys [difference] :as nearest} [ratio new-difference]]
-                                                            (if (< (Math/abs new-difference)
-                                                                   (Math/abs difference))
+                                                            (if (< (Math/abs (- 1 new-difference))
+                                                                   (Math/abs (- 1 difference)))
                                                               {:interval ratio
                                                                :difference new-difference}
                                                               nearest))
                                                           {:difference Float/MAX_VALUE}
                                                           limit-differences)]
                                               (if (and #_(<= 18 (Math/abs difference))
-                                                       (< (Math/abs (:difference new-nearest))
-                                                          (Math/abs difference)))
+                                                       (< (Math/abs (- 1 (:difference new-nearest)))
+                                                          (Math/abs (- 1 difference))))
                                                 (assoc new-nearest :harmonic (+ i 2))
                                                 nearest)))
                                           {:difference Float/MAX_VALUE}
                                           (map-indexed #(vector %1 %2) harmonic-limits))])
                 twelve-tets)))
 
-(->> twelve-tet->nearest-harmonic-limit
+#_(->> twelve-tet->nearest-harmonic-limit
      (map (fn [[s m]] (assoc m :semitones s)))
      (sort-by :harmonic)
      (group-by :harmonic))
+
+(->> twelve-tet->nearest-prime-limit
+     (map (fn [[i {:keys [map] :as m}]]
+            (assoc m
+                   :complexity (prime-limit-complexities map)
+                   :semitones i)))
+     (sort-by :complexity))
